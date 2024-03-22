e2c10123ce0d7124b655ed2917994a58df6b65b6
gaurabacharya <acharyaagaurab@gmail.com>
2024-03-22 13:50:24 -0700

    changed variables to camelCase in distancevector.py

--- src/distancevector.py
+++ src/distancevector.py
@@ -1,37 +1,51 @@
 class Node:
-    def __init__(self, node_id):
-        self.node_id = node_id
-        self.routing_table = {}  # destination: (next_hop, path_cost)
+    """
+    Represents a node in the network topology.
+
+    Attributes:
+    - nodeId: the unique identifier of the node
+    - routingTable: a dictionary of the routing table with the key as the 
+      destination and the next hop and pathCost as values
+    - neighbors: a dictionary of neighbor nodes and their associated costs
+
+    Methods:
+    - add_neighbor(neighborId, cost): add a neighbor node with the given cost
+    - remove_neighbor(neighborId): remove the neighbor node with the given id
+    - update_routing_table(destination, nextHop, pathCost): add an element to the routing table dictionary
+    """
+    def __init__(self, nodeId):
+        self.nodeId = nodeId
+        self.routingTable = {}  # destination: (nextHop, pathCost)
         self.neighbors = {}
 
-    def add_neighbor(self, neighbor_id, cost):
-        self.neighbors[neighbor_id] = cost
+    def add_neighbor(self, neighborId, cost):
+        self.neighbors[neighborId] = cost
 
-    def remove_neighbor(self, neighbor_id):
-        del self.neighbors[neighbor_id]
+    def remove_neighbor(self, neighborId):
+        del self.neighbors[neighborId]
 
-    def update_routing_table(self, destination, next_hop, path_cost):
-        self.routing_table[destination] = (next_hop, path_cost)
+    def update_routing_table(self, destination, nextHop, pathCost):
+        self.routingTable[destination] = (nextHop, pathCost)
             
 def read_topology_file(topology_file):
     nodes = {}
     with open(topology_file, 'r') as file:
         for line in file:
-            node_id, neighbor_id, cost = map(int, line.strip().split())
-            if node_id not in nodes:
-                nodes[node_id] = Node(node_id)
-            if neighbor_id not in nodes:
-                nodes[neighbor_id] = Node(neighbor_id)
-            nodes[node_id].add_neighbor(neighbor_id, cost)
-            nodes[neighbor_id].add_neighbor(node_id, cost)
+            nodeId, neighborId, cost = map(int, line.strip().split())
+            if nodeId not in nodes:
+                nodes[nodeId] = Node(nodeId)
+            if neighborId not in nodes:
+                nodes[neighborId] = Node(neighborId)
+            nodes[nodeId].add_neighbor(neighborId, cost)
+            nodes[neighborId].add_neighbor(nodeId, cost)
     return nodes          
 
 def read_message_file(message_file):
     msgs = []
     with open(message_file, 'r') as file:
         for line in file:
-            src_node_id, dest_node_id, msg_text = line.split(maxsplit=2)
-            msgs.append((int(src_node_id), int(dest_node_id), msg_text))
+            src_nodeId, dest_nodeId, msg_text = line.split(maxsplit=2)
+            msgs.append((int(src_nodeId), int(dest_nodeId), msg_text))
 
     return msgs
 
@@ -39,8 +53,8 @@ def read_topology_change_file(changes_file):
     changes = []
     with open(changes_file, 'r') as file:
         for line in file:
-            node_id, neighbor_id, cost = map(int, line.split(maxsplit=2))
-            changes.append((int(node_id), int(neighbor_id), cost))
+            nodeId, neighborId, cost = map(int, line.split(maxsplit=2))
+            changes.append((int(nodeId), int(neighborId), cost))
     return changes
 
 def bellman_ford(dst, routers, links):
@@ -58,59 +72,59 @@ def bellman_ford(dst, routers, links):
 
 def update_distance_vector(node, distance_vector, nexthop):
     for destination, cost in distance_vector.items():
-        # if destination != node.node_id and destination in nexthop and cost != float('inf'):
+        # if destination != node.nodeId and destination in nexthop and cost != float('inf'):
         if destination in nexthop and cost != float("inf"):
             nextNode = 0
-            if destination == node.node_id:
+            if destination == node.nodeId:
                 node.update_routing_table(destination, destination, 0)
-            elif nexthop[destination] == node.node_id:
+            elif nexthop[destination] == node.nodeId:
                 nextNode = destination
                 node.update_routing_table(destination, nextNode, cost)
             else:
                 nextNode = nexthop[destination] 
 
-                while nexthop[nextNode] != None and nexthop[nextNode] != node.node_id:
+                while nexthop[nextNode] != None and nexthop[nextNode] != node.nodeId:
                     if nextNode == None:
                         break
                     nextNode = nexthop[nextNode]
                 node.update_routing_table(destination, nextNode, cost)
         
 def run_bellman_ford(nodes, routers, links):
-    for node_id, node in nodes.items():
-        distance_vector, nexthop = bellman_ford(node_id, routers, links)
-        nexthop[node_id] = node_id
+    for nodeId, node in nodes.items():
+        distance_vector, nexthop = bellman_ford(nodeId, routers, links)
+        nexthop[nodeId] = nodeId
         update_distance_vector(node, distance_vector, nexthop)
         
 def write_routing_table(nodes, output_file):
     sorted_nodes = dict(sorted(nodes.items()))
-    for node_id, node in sorted_nodes.items():
-        sorted_table = sorted(node.routing_table.items(), key=lambda x: x[0])
+    for nodeId, node in sorted_nodes.items():
+        sorted_table = sorted(node.routingTable.items(), key=lambda x: x[0])
         with open(output_file, 'a') as f:
-            for dest, (next_hop, path_cost) in sorted_table:
-                f.write(f"{dest} {next_hop} {path_cost}\n")
+            for dest, (nextHop, pathCost) in sorted_table:
+                f.write(f"{dest} {nextHop} {pathCost}\n")
             f.write("\n")
 
 def get_links(nodes):
     links = []
-    for node_id, node in nodes.items():
-        for neighbor_id, cost in node.neighbors.items():
-            links.append((node_id, neighbor_id, cost))
+    for nodeId, node in nodes.items():
+        for neighborId, cost in node.neighbors.items():
+            links.append((nodeId, neighborId, cost))
     return links
 
 def write_messages(nodes, msgs):
     
     with open(output_file, 'a') as f:
         for msg in msgs:
-            if nodes[msg[1]].node_id not in nodes[msg[0]].routing_table.keys():
+            if nodes[msg[1]].nodeId not in nodes[msg[0]].routingTable.keys():
                 f.write(f"from {msg[0]} to {msg[1]} cost infinite hops unreachable message {msg[2]}")
             else:
                 hopString = ""
                 source = msg[0]
                 destination = msg[1]
-                pathCost = nodes[source].routing_table[destination][1]
+                pathCost = nodes[source].routingTable[destination][1]
                 nextHop = source
                 while nextHop != destination:
-                    nextHop = nodes[nextHop].routing_table[destination][0]
+                    nextHop = nodes[nextHop].routingTable[destination][0]
                     hopString = f"{hopString} {nextHop}"
                 hopString = f"{hopString} {nextHop}"
 
